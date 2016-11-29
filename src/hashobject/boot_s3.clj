38784f282407908d23bc498c9a3a56c6fcb14b65
Brent Hagany <brent@hiidef.com>
2016-11-29 07:00:55 -0600

    Make metadata and permissions separate task options

--- src/hashobject/boot_s3.clj
+++ src/hashobject/boot_s3.clj
@@ -7,24 +7,22 @@
 
 (def ^:private
   +defaults+ {:source "public"
-              :options {:permissions [[:all-users :read]]}})
+              :permissions [[:all-users :read]]})
 
 (boot/deftask s3-sync
-  "Sync local directory to AWS S3
-
-  The `options` parameter takes a map with two optional keys:
-   - `:metadata` a map with metadata to set on the objects, passed through to clj-aws-s3
-   - `:permissions` a seq of 2-tuples of `[grantee permission]`, passed through to clj-aws-s3"
-  [s source     PATH       str       "subdirectory in :target-path to upload to s3"
-   b bucket     BUCKET     str       "s3 bucket name"
-   a access-key ACCESS_KEY str       "s3 access key"
-   k secret-key SECRET     str       "s3 secret key"
-   o options    OPTIONS    edn       "metadata and permissions to apply to all synced objects"]
+  "Sync local directory to AWS S3"
+  [s source      PATH       str       "subdirectory in :target-path to upload to s3"
+   b bucket      BUCKET     str       "s3 bucket name"
+   a access-key  ACCESS_KEY str       "s3 access key"
+   k secret-key  SECRET     str       "s3 secret key"
+   m metadata    META       {kw str}  "a map with metadata to set on the objects, passed through to clj-aws-s3"
+   p permissions PERMS      [[kw kw]] "a seq of 2-tuples of `[grantee permission]`, passed through to clj-aws-s3"]
   (let [options (merge +defaults+ *opts*)
-        cred    (select-keys options [:access-key :secret-key])]
+        cred    (select-keys options [:access-key :secret-key])
+        s3-opts {:metadata metadata :permissions (:permissions options)}]
     (boot/with-post-wrap fileset
       (let [files (->> (boot/output-files fileset)
                        (boot/by-re [(re-pattern (str "^" (:source options)))]))]
         (u/info "Start upload to AWS S3.\n")
-        (s3/sync-to-s3 cred files (:source options) (:bucket options) (:options options))
+        (s3/sync-to-s3 cred files (:source options) (:bucket options) s3-opts)
         (u/info "Uploaded to AWS S3.\n")))))
