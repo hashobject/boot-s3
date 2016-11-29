081319da7bb4d4397c20cce01cea85b046b41f92
Brent Hagany <brent@hiidef.com>
2016-11-29 01:15:26 -0600

    Add task option to force upload; useful for header changes, which don't change the md5

--- src/hashobject/boot_s3.clj
+++ src/hashobject/boot_s3.clj
@@ -15,12 +15,13 @@
    b bucket     BUCKET     str       "s3 bucket name"
    a access-key ACCESS_KEY str       "s3 access key"
    k secret-key SECRET     str       "s3 secret key"
-   o options    OPTIONS    {str str} "Extra options set for each s3 file"]
+   o options    OPTIONS    {str str} "Extra options set for each s3 file"
+   f force                 bool      "Set to `true` to force upload of all objects"]
   (let [options (merge +defaults+ *opts*)
         cred    (select-keys options [:access-key :secret-key])]
     (boot/with-post-wrap fileset
       (let [files (->> (boot/output-files fileset)
                        (boot/by-re [(re-pattern (str "^" (:source options)))]))]
         (u/info "Start upload to AWS S3.\n")
-        (s3/sync-to-s3 cred files (:source options) (:bucket options) (:options options))
+        (s3/sync-to-s3 cred files (:source options) (:bucket options) (:options options) force)
         (u/info "Uploaded to AWS S3.\n")))))
